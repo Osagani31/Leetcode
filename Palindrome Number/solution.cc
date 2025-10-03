e34e1bbb0c276d13af6c03b6f54796be92fbaaab
Osagani31 <naonperera@students.nsbm.ac.lk>
2025-10-03 09:18:11 +0530

    Palindrome Number

---
+++ Palindrome Number/solution.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending in 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+
+        // Reverse only half of the number
+        while (x > reversed) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        // For even number of digits: x == reversed
+        // For odd number of digits: x == reversed/10
+        return (x == reversed || x == reversed / 10);
+    }
+};
